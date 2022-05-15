eecccf02a536aa3a913f024ad98ef55f59905567
“velsvelmurugan” <velmurugan.m@rorwebcoders.com>
2022-05-15 12:49:36 +0530

    final cleanup

--- db/migrate/20220515050304_devise_create_users.rb
+++ db/migrate/20220515050304_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name, null: false, default: ""
-
+      t.integer :role, null: false, default: 0
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
